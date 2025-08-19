b07e0231853732f6444a338ddf66133f89d3888b
Piotr <114903054+hepp3n@users.noreply.github.com>
2025-08-19 18:35:03 +0200

    update

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::collections::HashSet;
-
 use cosmic::app::{Core, Task};
-use cosmic::iced::futures::SinkExt;
+use cosmic::iced::futures::{SinkExt, StreamExt};
 use cosmic::iced::window::Id;
-use cosmic::iced::{stream, Limits, Subscription};
+use cosmic::iced::{self, stream, Limits, Subscription};
 use cosmic::iced_winit::commands::popup::{destroy_popup, get_popup};
 use cosmic::widget::{self, settings};
 use cosmic::{Application, Element};
-use kdeconnect::device::{ConnectedDevice, ConnectedDevices};
-use kdeconnect::{KdeConnectAction, KdeConnectClient};
-use tokio::sync::mpsc::channel;
+use kdeconnect::device::{ConnectedId, DeviceAction};
+use kdeconnect::KdeConnect;
 use tracing::info;
 
 use crate::fl;
@@ -21,9 +18,9 @@ pub struct CosmicConnect {
     core: Core,
     /// The popup id.
     popup: Option<Id>,
-    /// kdeconnect client
-    kdeconnect: Option<KdeConnectClient>,
-    connected_devices: ConnectedDevices,
+    /// KdeConnect client instance.
+    kdeconnect: Option<KdeConnect>,
+    connected_devices: Vec<ConnectedId>,
 }
 
 #[derive(Debug, Clone)]
@@ -31,15 +28,15 @@ pub enum Message {
     TogglePopup,
     PopupClosed(Id),
     KdeConnect(KdeConnectEvent),
-    DisconnectDevice,
-    PairDevice(String),
-    SendPing(String),
+    DisconnectDevice(ConnectedId),
+    PairDevice((ConnectedId, bool)),
+    SendPing((ConnectedId, String)),
 }
 
 #[derive(Debug, Clone)]
 pub enum KdeConnectEvent {
-    Connected(KdeConnectClient),
-    DevicesUpdated(ConnectedDevice),
+    Connected(KdeConnect),
+    DevicesUpdated(ConnectedId),
 }
 
 impl Application for CosmicConnect {
@@ -64,7 +61,7 @@ impl Application for CosmicConnect {
             core,
             popup: None,
             kdeconnect: None,
-            connected_devices: HashSet::new(),
+            connected_devices: Vec::new(),
         };
 
         (app, Task::none())
@@ -78,17 +75,21 @@ impl Application for CosmicConnect {
         Subscription::run_with_id(
             1,
             stream::channel(100, |mut output| async move {
-                let (device_tx, mut device_rx) = channel::<ConnectedDevice>(4);
-                let client = KdeConnectClient::new(device_tx).await.unwrap();
+                let (kdeconnect, mut devices) = KdeConnect::new();
+                let kconnect = kdeconnect.clone();
+
+                tokio::task::spawn(async move {
+                    kconnect.run_server().await;
+                });
 
                 let _ = output
-                    .send(Message::KdeConnect(KdeConnectEvent::Connected(client)))
+                    .send(Message::KdeConnect(KdeConnectEvent::Connected(kdeconnect)))
                     .await;
 
-                while let Some(devices) = device_rx.recv().await {
+                while let Some(devices) = devices.next().await {
                     let _ = output
                         .send(Message::KdeConnect(KdeConnectEvent::DevicesUpdated(
-                            devices,
+                            devices.0,
                         )))
                         .await;
                 }
@@ -96,7 +97,7 @@ impl Application for CosmicConnect {
         )
     }
 
-    fn view(&self) -> Element<Self::Message> {
+    fn view(&self) -> Element<'_, Self::Message> {
         self.core
             .applet
             .icon_button("display-symbolic")
@@ -104,25 +105,32 @@ impl Application for CosmicConnect {
             .into()
     }
 
-    fn view_window(&self, _id: Id) -> Element<Self::Message> {
-        let mut content_list = widget::list_column().add(settings::item(
-            fl!("applet-name"),
-            widget::button::standard("Disconnect").on_press(Message::DisconnectDevice),
-        ));
+    fn view_window(&self, _id: Id) -> Element<'_, Self::Message> {
+        let mut content_list = widget::list_column().add(widget::text(fl!("applet-name")));
 
         for connected in &self.connected_devices {
             content_list = content_list.add(settings::item_row(vec![
-                widget::text::title4(connected.name.clone()).into(),
+                widget::text::monotext(connected.clone()).into(),
+                widget::button::standard("Disconnect")
+                    .on_press(Message::DisconnectDevice(connected.clone()))
+                    .into(),
                 widget::button::standard("Pair")
-                    .on_press(Message::PairDevice(connected.id.clone()))
+                    .on_press(Message::PairDevice((connected.clone(), true)))
                     .into(),
                 widget::button::standard("Send Ping")
-                    .on_press(Message::SendPing(connected.id.clone()))
+                    .on_press(Message::SendPing((
+                        connected.clone(),
+                        "Hello From COSMIC APPLET!".to_string(),
+                    )))
                     .into(),
             ]));
         }
 
-        self.core.applet.popup_container(content_list).into()
+        self.core
+            .applet
+            .popup_container(content_list)
+            .limits(iced::Limits::NONE.max_width(680.0).max_height(800.0))
+            .into()
     }
 
     fn update(&mut self, message: Self::Message) -> Task<Self::Message> {
@@ -160,25 +168,25 @@ impl Application for CosmicConnect {
                         self.kdeconnect = Some(client);
                     }
                     KdeConnectEvent::DevicesUpdated(device) => {
-                        self.connected_devices.insert(device);
+                        self.connected_devices.push(device);
                     }
                 };
             }
-            Message::DisconnectDevice => {
+            Message::DisconnectDevice(id) => {
                 if let Some(client) = &self.kdeconnect {
-                    let _ = client.send_action(KdeConnectAction::Disconnect);
+                    client.send_action(id, DeviceAction::Disconnect);
                 }
                 self.kdeconnect = None;
                 self.connected_devices.clear();
             }
-            Message::PairDevice(_id) => {
+            Message::PairDevice((id, flag)) => {
                 if let Some(client) = &self.kdeconnect {
-                    let _ = client.send_action(KdeConnectAction::PairDevice);
+                    client.send_action(id, DeviceAction::Pair(flag));
                 }
             }
-            Message::SendPing(_id) => {
+            Message::SendPing((id, msg)) => {
                 if let Some(client) = &self.kdeconnect {
-                    let _ = client.send_action(KdeConnectAction::SendPing);
+                    client.send_action(id, DeviceAction::Ping(msg));
                 }
             }
         }
